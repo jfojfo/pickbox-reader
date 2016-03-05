297b26ef893d2a41d32804a056ac3036afe0ee45
jfo <jfojfo@gmail.com>
2016-03-05 22:25:17 +0800

    dark mode in left panel

--- src/components/HomeLeftPanel.vue
+++ src/components/HomeLeftPanel.vue
@@ -6,12 +6,31 @@
                 <li v-for="(cat, id) in cats" class="close-panel" @click="onCatClick(cat)">
                     <div class="item-content">
                         <div class="item-inner">
-                            <div class="item-title my-active"><span :class="{ 'my-active': isCat(cat) }">{{ cat }}</span></div>
+                            <div class="item-title my-active"><span
+                                    :class="{ 'my-active': isCat(cat) }">{{ cat }}</span></div>
                         </div>
                     </div>
                 </li>
             </ul>
         </div>
+
+        <!--<div class="content-block-title"></div>-->
+        <div class="list-block">
+            <ul>
+                <li class="item-content">
+                    <div class="item-inner">
+                        <div class="item-title">夜间模式</div>
+                        <div class="item-after">
+                            <label class="label-switch">
+                                <input type="checkbox" v-model="isDarkTheme">
+                                <div class="checkbox"></div>
+                            </label>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
     </div>
 </template>
 
@@ -21,9 +40,12 @@
     }
 </style>
 
-<script>
+<script type="text/ecmascript-6">
+    var storage = window.localStorage
+
     export default{
         data() {
+            var isDT = storage.getItem('isDarkTheme')
             return {
                 cats: {
                     '热门': 0,
@@ -34,13 +56,21 @@
                     '数码': 101050000,
                     '营销': 114000000
                 },
-                currentCat: '热门'
+                currentCat: '热门',
+                isDarkTheme: isDT != null && isDT != '0',
             }
         },
         computed: {
             currentCatId () {
                 return this.cats[this.currentCat]
             }
+        },
+        ready () {
+            this.$watch('isDarkTheme', (newValue, oldValue) => {
+                console.log(this.isDarkTheme)
+                storage.setItem('isDarkTheme', this.isDarkTheme ? '1' : '0')
+                this.$dispatch('dark-theme', this.isDarkTheme)
+            })
         },
         methods: {
             isCat (cat) {
