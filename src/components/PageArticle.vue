db2f37b7c496e0621a676dd67c0780a1a2033a09
jfo <jfojfo@gmail.com>
2016-03-06 11:49:05 +0800

    add right bottom ctl panel: back and setting

--- src/components/PageArticle.vue
+++ src/components/PageArticle.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="page" id="{{ htmlId }}">
-        <header class="bar bar-nav">
+        <header v-if="false" class="bar bar-nav">
             <a class="button button-link button-nav pull-left back" href="#" data-transition='slide-out'>
                 <span class="icon icon-left"></span>
                 返回
             </a>
-            <a @click="onMoreClick" class="icon icon2 icon2-more pull-right"></a>
+            <a @click="onMoreClick" class="icon myicon myicon-more pull-right"></a>
             <h1 class="title">正文</h1>
         </header>
-        <div v-if="article" class="content full-article">
+        <div v-if="article" @scroll="onScroll($event)" class="content full-article">
             <h2>{{ article.title }}</h2>
             <div class="article-info">
                 <span class="article-info-from">{{ article.feed_title }}</span>
@@ -21,6 +21,11 @@
             <div class="preloader"></div>
         </div>
 
+
+        <div v-if="!hideCtlPanel" class="bar bar-footer ctl-group pull-right" transition="ctl-group-fade">
+            <i @click="onBackClick" class="myicon myicon-back"></i>
+            <i @click="onMoreClick" class="myicon myicon-more"></i>
+        </div>
 
 
         <div class="popup-settings actions-modal" :class="[showSettings ? 'modal-in' : 'modal-out']">
@@ -70,7 +75,7 @@
 </template>
 
 <script type="text/ecmascript-6">
-    require('../css/ratchicons.less')
+    require('../css/iconfont.css')
 
     import Utils from './../utils/Utils.js'
     import API from './../api/LeanCloud.js'
@@ -90,7 +95,8 @@
             return {
                 article: this.args.article,
                 showSettings: false,
-                fontBase: 0,
+                hideCtlPanel: false,
+                fontBase: 0
             }
         },
 
@@ -166,12 +172,25 @@
                 }
             },
 
+            onBackClick () {
+                $.router.back()
+            },
+
             onMoreClick () {
                 this.showSettings = true
             },
 
             onClosePopup () {
                 this.showSettings = false
+            },
+
+            onScroll (e) {
+                var curr = e.currentTarget.scrollTop
+                console.log(curr, this.lastScrollTop)
+                if (this.lastScrollTop !== undefined) {
+                    this.hideCtlPanel = (curr - this.lastScrollTop > 0)
+                }
+                this.lastScrollTop = curr
             }
         }
     }
@@ -183,13 +202,14 @@
 
     .full-article {
         padding: 0 0.5rem;
+
         img {
-             max-width: 100%;
+            max-width: 100%;
         }
     }
 
     /*.full-article img {*/
-        /*max-width: 100%;*/
+    /*max-width: 100%;*/
     /*}*/
 
     .article-info {
@@ -216,4 +236,41 @@
     .popup-settings .list-block {
         margin: 1rem 0;
     }
+
+    .ctl-group {
+        background-color: transparent;
+        left: auto;
+        margin-bottom: 1rem;
+        margin-right: 0.5rem;
+    }
+
+    .ctl-group-fade-transition {
+        opacity: 1;
+        transition: opacity .4s;
+    }
+
+    .ctl-group-fade-leave {
+        opacity: 0;
+    }
+
+    .ctl-group-fade-enter {
+        opacity: 0;
+    }
+
+    .ctl-group i {
+        font-size: 1rem;
+        width: 2rem;
+        height: 2rem;
+        /*margin: 10px;*/
+        /*padding: 10px;*/
+        border-radius: 6px;
+        background-color: #ddd;
+
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        margin-left: 0.5rem;
+        opacity: 0.9;
+    }
+
 </style>
